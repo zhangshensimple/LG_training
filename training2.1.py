e098f8dfc87ed3f0dfcb68e2b3e2c0540ad45d93
zstone <edz@zhangshendeMacBook-Pro.local>
2022-05-28 20:55:35 +0800

    Initial commit

---
+++ training2.1.py
@@ -0,0 +1,23 @@
+# encoding=utf-8
+
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+
+# 定义color接口，参数para 值为0 返回red，值为1 返回green 值为1 返回yello
+@app.route('/color', methods=["GET"])
+def color():
+    para = request.args.get("para")
+    if para == "0":
+        return jsonify(code=0, msg="red")
+    elif para == "1":
+        return jsonify(code=1, msg="green")
+    elif para == "2":
+        return jsonify(code=2, msg="yellow")
+    else:
+        return jsonify(code=-1, msg="illegal")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
