39dda65daca6bf73129eb125a23d8fbbdcb29bda
zhangshen <zhangshensimple@foxmail.com>
2022-06-04 18:16:33 +0800

    将参数值定义为整型 参数值不为整型时的容错处理

--- tra.py
+++ tra.py
@@ -1,12 +1,8 @@
-s1='hello world'
-s2 = "Hello Nowcoder"
-print(s1 + '\n' + s2)
-
-name = input("输入name")
-print("I am  {0} I am studying Python in Nowcoder!" .format(name))
-print(f"I am  {name} I am studying Python in Nowcoder!")
-print(name.upper())
-print(name.lower())
-# print(upper(name[0]))
-print(name.title())
-print(name[1::])
+pizza_inventory=['durian','chicken','durian']
+for i in pizza_inventory:
+    if i == 'bacon':
+        pizza_inventory.remove(i)
+        print('A bacon pizza was deleted from list.')
+        break
+    else:
+        print('There is really no bacon in pizza_inventory!')
